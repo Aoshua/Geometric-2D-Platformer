e86e580905de512e6cd033ca6ec1217edf2b719d
Josh Abbott <39091681+Aoshua@users.noreply.github.com>
2024-10-22 13:52:35 -0600

    why button no worky??

--- src/ui/menus/main_menu.gd
+++ src/ui/menus/main_menu.gd
@@ -12,3 +12,7 @@ func _on_continue_button_pressed():
 
 func _on_select_level_button_pressed():
 	pass # Replace with function body.
+
+func _unhandled_input(event):
+	if event is InputEventScreenTouch:
+		print("Screen touched at position: ", event.position)
