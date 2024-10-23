1766a3f44f2d58a222881b287a912725e69ee749
Josh Abbott <39091681+Aoshua@users.noreply.github.com>
2024-10-23 14:18:29 -0600

    level select screen

---
+++ src/ui/level_button.gd
@@ -0,0 +1,26 @@
+extends Button
+
+var level_number = 0
+var level_scene = ""
+var is_unlocked = false
+
+# Function to set up the button with level info
+func set_level(number: int, unlocked: bool):
+	level_number = number
+	level_scene = "res://src/levels/level_" + str(number) + ".tscn"
+	is_unlocked = unlocked
+	
+	# Set button text
+	text = str(level_number)
+	
+	# Disable or enable the button depending on whether it's unlocked
+	if is_unlocked:
+		disabled = false
+	else:
+		disabled = true
+		# Optionally, change the appearance of the button to show it's locked
+		add_theme_color_override("font_color", Color(0.5, 0.5, 0.5))  # Grey out text
+
+func _pressed():
+	if is_unlocked:
+		get_tree().change_scene_to_file(level_scene)
--- src/ui/menus/main_menu.gd
+++ src/ui/menus/main_menu.gd
@@ -9,17 +9,9 @@ func _on_continue_button_pressed():
 
 
 func _on_select_level_button_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://src/ui/menus/level_select.tscn")
 
 
 func _unhandled_input(event):
 	if event is InputEventScreenTouch:
 		print("Screen touched at position: ", event.position)
-
-
-func _on_continue_button_mouse_entered():
-	print("Mouse entered")
-
-
-func _on_continue_button_mouse_exited():
-	print("Mouse exited")
