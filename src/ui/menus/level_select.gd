931c61d775e299935fcf010b43dfb4b96d155406
Josh Abbott <39091681+Aoshua@users.noreply.github.com>
2024-10-23 16:02:49 -0600

    prog to add paging to level selection scene

--- src/ui/menus/level_select.gd
+++ src/ui/menus/level_select.gd
@@ -2,11 +2,40 @@ extends CanvasLayer
 
 var levels_path = "res://src/levels"
 var total_levels = 7
+var rows = 3
+var columns = 6
+var levels_per_page = rows * columns
+var current_page = 0
+var total_pages = int(ceil(float(total_levels) / levels_per_page))
 
 @onready var level_button_scene = preload("res://src/ui/level_button.tscn")
 
 func _ready():
-	for i in range(total_levels):
+	update_grid()
+
+
+func _on_left_button_pressed():
+	if current_page > 0:
+		current_page -= 1
+		update_grid()
+
+
+func _on_right_button_pressed():
+	if current_page < total_pages - 1:
+		current_page += 1
+		update_grid()
+
+
+func update_grid():
+	# Empty grid of old buttons
+	for child in %GridContainer.get_children():
+		%GridContainer.remove_child(child)
+		child.queue_free()  # Free the memory
+	
+	var start_index = current_page * levels_per_page
+	var end_index = min(start_index + levels_per_page, total_levels)
+	
+	for i in range(start_index, end_index):
 		var level_button = level_button_scene.instantiate()
 
 		# Determine if this level is unlocked
@@ -16,3 +45,10 @@ func _ready():
 			level_button.set_level(i + 1, false)  # Locked
 
 		%GridContainer.add_child(level_button)
+	
+	update_button_states()
+
+
+func update_button_states():
+	%LeftButton.visible = (current_page != 0)
+	%RightButton.visible = (current_page != total_pages - 1)
