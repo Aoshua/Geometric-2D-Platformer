643539984943aef05236eb281b0c45429fe882be
Josh Abbott <39091681+Aoshua@users.noreply.github.com>
2024-10-24 16:04:31 -0600

    began adding a pause button

---
+++ src/ui/menus/pause_menu.gd
@@ -0,0 +1,11 @@
+extends CanvasLayer
+
+
+func _on_reset_button_pressed():
+	get_tree().reload_current_scene()
+	queue_free()
+
+
+func _on_main_menu_button_pressed():
+	get_tree().change_scene_to_file("res://src/ui/menus/main_menu.tscn")
+	queue_free()
