1c45ee989e64858adf8b76a5610c12f125f558e0
Josh Abbott <39091681+Aoshua@users.noreply.github.com>
2024-10-25 16:03:11 -0600

    flsdfksdfsd;kfja;sdlfkja;sdlfkjasdlkfjksdjfjfjfjjfjfjfjfjfj

--- src/ui/menus/game_over.gd
+++ src/ui/menus/game_over.gd
@@ -1,15 +1,13 @@
 extends CanvasLayer
 
-
 var rewarded_ad : RewardedAd
 var rewarded_ad_load_callback := RewardedAdLoadCallback.new()
 var on_user_earned_reward_listener := OnUserEarnedRewardListener.new()
-
+var full_screen_content_callback := FullScreenContentCallback.new()
 
 func _on_reset_button_pressed():
 	get_tree().reload_current_scene()
 	queue_free()
-
 
 func _on_revive_button_pressed():
 	print("Revive button pressed")
@@ -29,30 +27,31 @@ func _on_revive_button_pressed():
 	rewarded_ad_load_callback.on_ad_loaded = on_rewarded_ad_loaded
 	on_user_earned_reward_listener.on_user_earned_reward = on_user_earned_reward
 
-
 func _on_main_menu_button_pressed():
 	get_tree().change_scene_to_file("res://src/ui/menus/main_menu.tscn")
 	queue_free()
 
-
 func on_rewarded_ad_failed_to_load(adError : LoadAdError) -> void:
 	print(adError.message)
 
-
 func on_rewarded_ad_loaded(loaded_ad : RewardedAd) -> void:
-	print("Rewarded ad loaded successfully")
 	self.rewarded_ad = loaded_ad
+
+	# Assign the FullScreenContentCallback here
+	rewarded_ad.full_screen_content_callback = full_screen_content_callback
+
+	# Set up the full screen content callback
+	full_screen_content_callback.on_ad_dismissed_full_screen_content = func() -> void:
+		print("Ad dismissed, cleaning up")
+		revive_player()  # Revive the player here when ad is dismissed
 
 	# Show the ad once it's loaded
 	if rewarded_ad:
-		rewarded_ad.show()
-
+		rewarded_ad.show(on_user_earned_reward_listener)
 
 func on_user_earned_reward(rewarded_item : RewardedItem) -> void:
 	# This function is called once the ad is successfully watched
-	print("User earned reward, reviving player...")
-	revive_player()
-
+	print("User earned reward...")
 
 func revive_player():
 	print("Reviving player...")
