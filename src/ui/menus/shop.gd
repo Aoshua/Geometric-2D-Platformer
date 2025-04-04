36d479c09f21f05a4e3f52d484b459f32d178792
Josh Abbott <39091681+Aoshua@users.noreply.github.com>
2025-04-03 20:13:52 -0600

    #6 item labels showing

--- src/ui/menus/shop.gd
+++ src/ui/menus/shop.gd
@@ -1,16 +1,40 @@
 extends CanvasLayer
 
-func _ready():
-	var header_height = $Header.size.y + 40 # 40 is arbitrary offset
-	var screen_size = get_viewport().get_visible_rect().size
-	
-	# Set TabContainer position and size
-	$TabContainer.anchor_left = 0
-	$TabContainer.anchor_top = 0
-	$TabContainer.anchor_right = 1
-	$TabContainer.anchor_bottom = 1
-	
-	$TabContainer.offset_left = 10
-	$TabContainer.offset_top = header_height
-	$TabContainer.offset_right = -10
-	$TabContainer.offset_bottom = -10
+#func _ready():
+	#set_scroll_container_dimensions()
+
+
+func set_scroll_container_dimensions():
+	# Get references to containers
+	var scroll_container = $TabContainer/Items/ScrollContainer
+	var hbox = $TabContainer/Items/ScrollContainer/ItemsGrid
+
+	# Make ScrollContainer fill the tab area
+	scroll_container.anchor_left = 0
+	scroll_container.anchor_top = 0
+	scroll_container.anchor_right = 1
+	scroll_container.anchor_bottom = 1
+	scroll_container.offset_left = 0
+	scroll_container.offset_top = 0
+	scroll_container.offset_right = 0
+	scroll_container.offset_bottom = 0
+
+	# Important: Enable size flags to expand
+	scroll_container.size_flags_horizontal = Control.SIZE_FILL
+	scroll_container.size_flags_vertical = Control.SIZE_FILL
+
+	# Set up the HBoxContainer with margins
+	hbox.anchor_left = 0
+	hbox.anchor_top = 0
+	hbox.anchor_right = 1
+	hbox.anchor_bottom = 1
+
+	# Apply the 15px offset around all sides
+	hbox.offset_left = 15
+	hbox.offset_top = 15
+	hbox.offset_right = -15
+	hbox.offset_bottom = -15
+
+	# Make HBoxContainer expand to fill the ScrollContainer
+	hbox.size_flags_horizontal = Control.SIZE_FILL
+	hbox.size_flags_vertical = Control.SIZE_FILL
