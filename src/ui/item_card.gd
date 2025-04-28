077d051249d85aede6eeab768217fb543974833c
Josh Abbott <39091681+Aoshua@users.noreply.github.com>
2025-04-28 11:17:28 -0600

    #6 shop card "component"

---
+++ src/ui/item_card.gd
@@ -0,0 +1,22 @@
+# item_card.gd
+extends Control
+
+signal buy_pressed(item_id)
+
+@export var item_name: String = "Item Name" 
+@export var item_price: int = 100
+@export var item_icon: Texture2D
+@export var item_id: String = ""
+
+func _ready():
+	update_display()
+
+func update_display():
+	$PanelContainer/MarginContainer/VBoxContainer/ItemName.text = item_name
+	$PanelContainer/MarginContainer/VBoxContainer/PriceContainer/ItemPrice.text = str(item_price)
+	  
+	if item_icon:
+		$PanelContainer/MarginContainer/VBoxContainer/CenterContainer/ItemSprite.texture = item_icon
+
+func _on_buy_button_pressed():
+	emit_signal("buy_pressed", item_id)
--- src/ui/menus/shop.gd
+++ src/ui/menus/shop.gd
@@ -1,7 +1,66 @@
 extends CanvasLayer
 
+
+var item_card_scene = preload("res://src/ui/item_card.tscn")
+var shop_items = [
+	{
+		"id": "shield",
+		"name": "Shield", 
+		"price": 500,
+		"icon": preload("res://assets/force-field.png")
+	},
+	{
+		"id": "shield2",
+		"name": "Shield 2", 
+		"price": 500,
+		"icon": preload("res://assets/force-field.png")
+	},
+		{
+		"id": "shield",
+		"name": "Shield", 
+		"price": 500,
+		"icon": preload("res://assets/force-field.png")
+	},
+	{
+		"id": "shield2",
+		"name": "Shield 2", 
+		"price": 500,
+		"icon": preload("res://assets/force-field.png")
+	},
+		{
+		"id": "shield",
+		"name": "Shield", 
+		"price": 500,
+		"icon": preload("res://assets/force-field.png")
+	},
+	{
+		"id": "shield2",
+		"name": "Shield 2", 
+		"price": 500,
+		"icon": preload("res://assets/force-field.png")
+	},
+]
+
+
 func _ready():
 	%CurrentCoinsLabel.text = Formatters.format_number_with_commas(Global.coins)
+	var items_grid = $ShopContainer/ItemsContainer/ScrollContainer/MarginContainer/ItemsGrid
+	
+	for item in shop_items:
+		var card = item_card_scene.instantiate()
+		card.item_id = item.id
+		card.item_name = item.name
+		card.item_price = item.price
+		card.item_icon = item.icon
+		card.connect("buy_pressed", _on_item_buy_pressed)
+		items_grid.add_child(card)
+
+
+func _on_item_buy_pressed(item_id):
+	print("Bought item: " + item_id)
+	# Check if player has enough coins
+	# Deduct coins
+	# Add item to inventory
 
 
 func _on_touch_screen_button_pressed():
