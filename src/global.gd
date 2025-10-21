85f3e9378aa80021981e8ba0834d3386a68c215d
Josh Abbott <39091681+Aoshua@users.noreply.github.com>
2025-10-21 15:21:14 -0600

    #28 new screenshots

--- src/global.gd
+++ src/global.gd
@@ -8,9 +8,8 @@ const TOTAL_LEVELS = 7
 # Path to the save file. For mobile and desktop, this saves to a user-specific directory.
 const SAVE_PATH = "user://save_game.json"
 # Used to set the window size and appropriately scale screenshots for iOS
-#const RES_IOS_6_9 = Vector2i(2796, 1290)
-#const RES_IOS_6_5 = Vector2i(2688, 1242)
-const RES_IPAD_13 = Vector2i(2752, 2064)
+const RES_IOS_6_5 = Vector2i(2688, 1242)
+#const RES_IPAD_13 = Vector2i(2752, 2064)
 
 # Save Data:
 var unlocked_levels = 1
@@ -21,9 +20,9 @@ var unlocked_skins = [PlayerSkins.GREEN]
 
 # Only needed when taking screenshots
 # Also be sure to enable Project Settings > Display > Window :
-# Stretch, Mode=viewport Aspect=expand
+# Stretch > Mode=viewport & Aspect=expand
 #func _ready():
-	#get_window().size = RES_IPAD_13
+	#get_window().size = RES_IOS_6_5
 	#get_window().move_to_center()
 
 
@@ -141,8 +140,8 @@ func _input(event):
 			
 			var current_size = img.get_size()
 			# Scale to target size
-			if current_size != RES_IPAD_13:
-				img.resize(RES_IPAD_13.x, RES_IPAD_13.y)
+			if current_size != RES_IOS_6_5:
+				img.resize(RES_IOS_6_5.x, RES_IOS_6_5.y)
 			
 			var file_path = "res://publishing/screenshots/screenshot_%d.png" % Time.get_ticks_msec()
 			var err = img.save_png(file_path)
